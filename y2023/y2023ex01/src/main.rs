155ff727972e138f76449b9c1f828aab391627c2
Roberto Gambuzzi <gambuzzi@gmail.com>
2023-12-02 12:01:37 +0000

    add tests y2023ex01

--- y2023/y2023ex01/src/main.rs
+++ y2023/y2023ex01/src/main.rs
@@ -67,3 +67,21 @@ fn main() -> Result<(), std::io::Error> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = include_str!("../input.txt");
+
+    #[test]
+    fn test_part1() {
+        let lines: Vec<String> = INPUT.lines().map(|line| line.to_string()).collect();
+        assert_eq!(solve(&lines, true), 53386);
+    }
+
+    #[test]
+    fn test_part2() {
+        let lines: Vec<String> = INPUT.lines().map(|line| line.to_string()).collect();
+        assert_eq!(solve(&lines, false), 53312);
+    }
+}
