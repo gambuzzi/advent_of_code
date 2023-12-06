bbf4fa6537186d7ce65733f240e1418a25079a6d
Roberto Gambuzzi <gambuzzi@gmail.com>
2023-12-06 05:35:28 +0000

    add y2023ex06 in python

--- y2023/y2023ex05/benches/bench_y2023ex05.rs
+++ y2023/y2023ex05/benches/bench_y2023ex05.rs
@@ -4,9 +4,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let input: &str = include_str!("../input.txt");
-    let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
-    c.bench_function("y2023ex05::part1", |b| b.iter(|| part1(black_box(&lines))));
-    c.bench_function("y2023ex05::part2", |b| b.iter(|| part2(black_box(&lines))));
+    c.bench_function("y2023ex05::part1", |b| b.iter(|| part1(black_box(input))));
+    c.bench_function("y2023ex05::part2", |b| b.iter(|| part2(black_box(input))));
 }
 
 criterion_group!(benches, criterion_benchmark);
